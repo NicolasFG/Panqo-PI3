8a91bbf2be62556d46591a5790c97884bbd140dd
jorgevasquezutec <jorge.vasquez@utec.edu.pe>
2021-07-26 02:21:23 -0500

    change env port

--- local.js
+++ local.js
@@ -1,6 +1,6 @@
 require('dotenv').config();
 const app = require('./app/app.js');
-const port = process.env.SERVER_PORT || 8000;
+const port = process.env.PORT || 8000;
 // console.log(process);
 // Server
 app.listen(port, () => {
