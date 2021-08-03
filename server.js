cc0385db784665b5a3b010216ac8184ddfc97660
jorgevasquezutec <jorge.vasquez@utec.edu.pe>
2021-08-03 11:51:14 -0500

    edit node js

--- server.js
+++ server.js
@@ -1,12 +1,13 @@
- const express = require('express');
- const serveStatic = require("serve-static")
- const path = require('path');
- app = express();
- app.use(serveStatic(path.join(__dirname, 'dist')));
+const path = require('path');
+const express = require('express');
+const app = express();
 
+// Serve static files
+app.use(express.static(__dirname + '/dist'));
+
+// Send a
  app.get('/*', function(req, res) {
     res.sendFile(path.join(__dirname + '/dist/index.html'));
   });
 
- const port = process.env.PORT || 80;
- app.listen(port);
+  app.listen(process.env.PORT || 5000);
